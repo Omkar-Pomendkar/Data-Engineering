62352a149d063008bbaaac46d4d2cf401cc7df3c
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-11-21 23:54:58 +0530

    Dictionary

---
+++ python/Revision/5 - Dicitionary & Sets/dictionary_methods.py
@@ -0,0 +1,17 @@
+d = {
+    "fast":"In a quick Manner",
+    "Omkar":"A Coder",
+    "Marks" : [1,2,3,4],
+    "Another Dict" : { 1: "One"}
+}
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# print(d.pop("fast"))
+# print(d)
+# print(d.popitem())
+# print(d.popitem())
+# print(d.popitem())
+print(d.get("fast"))
+print(d)
+
