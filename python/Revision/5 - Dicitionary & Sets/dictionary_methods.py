5c2e7e8bb73c3731d661967b0e77557f16ff7053
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-11-22 20:50:29 +0530

    dictionary

--- python/Revision/5 - Dicitionary & Sets/dictionary_methods.py
+++ python/Revision/5 - Dicitionary & Sets/dictionary_methods.py
@@ -6,7 +6,7 @@ d = {
 }
 # print(d.keys())
 # print(d.values())
-# print(d.items())
+print(d.items())   # Returns Tuple for all contains (key,value)
 # print(d.pop("fast"))
 # print(d)
 # print(d.popitem())
@@ -14,4 +14,15 @@ d = {
 # print(d.popitem())
 print(d.get("fast"))
 print(d)
+# Update 
+ud = {
+    "name" : "omkar"
+}
+d.update(ud) # Update the Dictionary for Adding Dictionary
+print(d)
+
+# differencr between .get and [] bracket Syntax
+print(d["name1"])  # Here you know name is present in Dicitionary
+print(d.get("name1"))   # Returns None as name1 is not present in Dictionary
+
 
