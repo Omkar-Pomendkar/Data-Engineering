7a8ae430b4921e96aad9705df7d1b9f9dfe6b352
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-11-30 21:08:58 +0530

    Updated

--- Database/Sql/test.py
+++ Database/Sql/test.py
@@ -1,14 +1,15 @@
 import mysql.connector as conn
 
 mydb = conn.connect(host = "localhost" , user ="root" , passwd = "sS123456%" )
-#print(mydb)
-cursor = mydb.cursor()
+print(mydb)
+conn.close()
+# cursor = mydb.cursor()
 #cursor.execute("create database Omkar")
 
 #s = "create table Omkar.omkardetails(employe_id int(10)  , emp_name varchar(80) , emp_mailid varchar(20),emp_salary int(6) , emp_attendence int(3))"
 #q1 = cursor.execute(s)
 
-q2 = cursor.execute("select * from Omkar.omkardetails")
-print(q2)
+# q2 = cursor.execute("select * from Omkar.omkardetails")
+# print(q2)
 
 
--- python/Revision/8 - Functions & Recursion/function.py
+++ python/Revision/8 - Functions & Recursion/function.py
@@ -1,8 +1,14 @@
 # Create a function to add 2 Numbers
 
+l = [1,2,3,4,5,6,7,8]
+
+
 def sum(a,b):
     c = a+b
     return c 
 
 d = sum(9,8)
 print(d)
+
+def avg (num,l):
+    result = 
